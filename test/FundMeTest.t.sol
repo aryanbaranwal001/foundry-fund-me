808511395d15a866457c88d90f4964de6f70eb56
aryanbaranwal001 <aryanbaranwal131214@gmail.com>
2025-03-06 01:45:58 +0530

    modified and refractored fundmetest

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "lib/forge-std/src/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+
+contract FundMeTest is Test {
+  FundMe fundMe;
+
+  function setUp() external {
+    DeployFundMe deployFundMe = new DeployFundMe();
+    fundMe = deployFundMe.run();
+  }
+
+  function testMinimumUsd() view external  {
+    assertEq(fundMe.MINIMUM_USD(), 5e18);
+  }
+  function testOwnerIsMsgSender() public view {
+    assertEq(fundMe.i_owner(), msg.sender);
+  }
+
+  function testPriceFeedVersionIsAccurate() view public {
+    uint256 version = fundMe.getVersion();
+    assertEq(version, 4);
+  }
+
+}
